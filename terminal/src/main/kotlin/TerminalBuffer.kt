79137f2f3395acbdc57e91cfe7f64a9635aeafbc
Dimitrije-Stoilkovic <dimitrije.stoilkovic@gmail.com>
2026-03-15 16:22:58 +0100

    "Added write, fill, cursor operations and content access methods"

--- terminal/src/main/kotlin/TerminalBuffer.kt
+++ terminal/src/main/kotlin/TerminalBuffer.kt
@@ -66,5 +66,52 @@
         }
     }
 
-    
+    fun write (char: Char?){
+        val cell = screen.get(cursorRow).cells[cursorCol]
+        cell.char = char
+        cell.applyAttributes(currentAttributes)
+        if (cursorCol + 1 == width) {
+            if (cursorRow + 1 == height) {
+                insertEmpty()
+                setCursorPosition(0, height - 1)
+            } else {
+                setCursorPosition(0, cursorRow + 1)
+            }
+        } else {
+            moveCursorRight(1)
+        }
+    }
+
+    fun writeText(text: String){
+        for (char in text){
+            write(char)
+        }
+    }
+
+    fun fill(char: Char? = null){
+        setCursorPosition(0, cursorRow)
+        repeat(width) {
+            write(char)
+        }
+    }
+
+    fun getCharScreen(row: Int, col: Int): Char? {
+        if (col < 0 || col >= width || row < 0 || row >= height) return null
+        return screen.get(row).cells[col].char
+    }
+
+    fun getCharScrollback(row: Int, col: Int): Char? {
+        if (col < 0 || col >= width || row < 0 || row >= scrollback.size) return null
+        return scrollback.get(row).cells[col].char
+    }
+
+    fun getAttributesScreen(row: Int, col: Int): Attributes? {
+        if (col < 0 || col >= width || row < 0 || row >= height) return null
+        return screen.get(row).cells[col].toAttributes()
+    }
+
+    fun getAttributesScrollback(row: Int, col: Int): Attributes? {
+        if (col < 0 || col >= width || row < 0 || row >= scrollback.size) return null
+        return scrollback.get(row).cells[col].toAttributes()
+    }
 }
